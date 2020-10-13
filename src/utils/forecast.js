fc3e0d5ea56c57af4abeee6161a677bee2c9ba14
Nick Honold <nhonold81@gmail.com>
2020-10-13 13:19:30 -0400

    added humidity text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
       } else if (body.error) {
          callback('Unable to find location', undefined)
       } else {
-         callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. The temperature feels like ' + body.current.feelslike + ' degrees outside.')
+         callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. The temperature feels like ' + body.current.feelslike + ' degrees outside. The humidity is ' +body.current.humidity + ' percent.' )
       }
     })
  }
